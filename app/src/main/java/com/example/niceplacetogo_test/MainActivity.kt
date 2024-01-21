0389b618ffb499720afa3b7db660b94c82632652
Karin <kf1@gmx.at>
2024-01-22 00:01:59 +0100

    some changes

--- app/src/main/java/com/example/niceplacetogo_test/MainActivity.kt
+++ app/src/main/java/com/example/niceplacetogo_test/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.niceplacetogo_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find views by ID
+        val btnShoot = findViewById<Button>(R.id.btnShoot)
+
+        // Rotate Icon
+        val ivCamera = findViewById<ImageView>(R.id.ivCamera)
+        //val rotation = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        //ivCamera.startAnimation(rotation)
+
+        // Set OnClickListener
+        btnShoot.setOnClickListener {
+            // Show Signed-In-Message
+            Toast.makeText(this, getString(R.string.click), Toast.LENGTH_LONG).show()
+
+            // Open ListActivity
+            //val intent = Intent(this, ListActivity::class.java)
+            //startActivity(intent)
+        }
     }
 }
